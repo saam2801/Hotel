6e402dd94983d0b6c0b09e5358114b507a9573e6
saam2801 <samugsilva2017@gmail.com>
2025-10-31 20:51:41 -0300

    Adição da tela sobre

--- Hotel/Views/ContratacaoHospedagem.xaml.cs
+++ Hotel/Views/ContratacaoHospedagem.xaml.cs
@@ -6,4 +6,9 @@ public partial class ContratacaoHospedagem : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        App.Current.MainPage = new Sobre();
+    }
 }
