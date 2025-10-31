1c86431a6929615758ea9e4aa372a3ca4cae4eff
saam2801 <samugsilva2017@gmail.com>
2025-10-31 07:37:18 -0300

    Adicionar arquivos de projeto.

---
+++ Hotel/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Hotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Height = 600;
+            window.Width = 400;
+
+            return window;
+        }
+    }
+}
